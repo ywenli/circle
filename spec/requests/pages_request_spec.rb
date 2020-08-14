8159d8f32002a68da153b96dcdb5a96671c17b5c
ywenli <kuota0204@gmail.com>
2020-08-14 13:49:45 +0900

    Add rspec test for PagesController #1

--- spec/requests/pages_request_spec.rb
+++ spec/requests/pages_request_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
-
+  describe 'GET #home' do
+    it 'returns http success' do
+      get '/'
+      expect(response.status).to eq 200
+    end
+  end
 end
