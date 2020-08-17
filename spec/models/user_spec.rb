96796c9b9febbcc9783163f3b164c0aef370379d
ywenli <kuota0204@gmail.com>
2020-08-17 14:32:32 +0900

    Add test for username #8

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { User.new( username: "Example",
+                        email: "user@example.com",
+                        password: "foobar",
+                        password_confirmation: "foobar")}
+
+  describe 'User' do
+    it 'should be valid' do
+      expect(user).to be_valid
+    end
+  end
+
+  describe 'username' do
+    it 'should be presence' do
+      user.username = " "
+      expect(user).to be_invalid
+    end
+    context '50 characters' do
+      it 'is valid' do
+        user.username = "a" * 50
+        expect(user).to be_valid
+      end
+    end
+    context '51 characters' do
+      it 'is invalid' do
+        user.username = "a" * 51
+        expect(user).to be_invalid
+      end
+    end
+  end
 end
