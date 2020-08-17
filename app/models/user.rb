a56d00e2d16e55780df3ad66dd976658eabe91d5
ywenli <kuota0204@gmail.com>
2020-08-17 15:38:14 +0900

    Add validation: save as downcase #8

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, presence: true, uniqueness: true, length: { maximum: 50 }
+  before_save :downcase_email
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX}
+
+  validates :username, presence: true, uniqueness: true, length: { maximum: 50 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX}
+
+  private
+
+    def downcase_email
+      email.downcase!
+    end
 
 end
 
