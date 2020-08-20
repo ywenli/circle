17165074f2e52fc33c1e5aacd16025eac2deffb4
ywenli <kuota0204@gmail.com>
2020-08-20 12:45:03 +0900

    Fix error messages collapse #24

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :username, presence: true, uniqueness: true, length: { maximum: 50 }
-  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX}
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX}, allow_blank: true
 
   private
 
