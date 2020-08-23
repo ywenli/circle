834ea838d579769bf795f0c0745a23449a75cf66
ywenli <kuota0204@gmail.com>
2020-08-23 17:30:57 +0900

    Add action: after_update_path #17

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -65,4 +65,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
       resource.update_without_password(params)
     end
 
+    # アカウント編集後、プロフィール画面に移動する
+    def after_update_path_for(resource)
+      user_path(id: current_user.id)
+    end
+
 end
